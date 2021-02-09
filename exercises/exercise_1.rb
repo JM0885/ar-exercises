1c0ea99b18eec0d49bf2a789c019a854ff83bb2e
JM0885 <jared.mosher@gmail.com>
2021-02-09 17:08:46 +0000

    Add store information using Create

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,13 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+### Exercise 1: Create 3 Stores
+
+Store.create([
+  { name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true },
+  { name: 'Richmond' annual_revenue: 1260000, mens_apparel: false, womens_apparel: true },
+  { name: 'Gastown' annual_revenue: 190000, mens_apparel: true, womens_apparel: false },
+  { name: 'Gastown2' annual_revenue: 190000, mens_apparel: false, womens_apparel: false }
+])
+
+puts Store.count
